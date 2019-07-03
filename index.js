25909da7c45f3c8411dc6e3ae76a6badcf37ec47
Thomas Varo <zalipsn@gmail.com>
2019-07-03 15:59:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.console.error("what the hell am i doing");
+console.console.error("what the hell am i doing")
+console.console.log("What am indoing here");
+console.console.warn("STAAAAAP");
