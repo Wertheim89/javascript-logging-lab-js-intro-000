05e928b1e56d6df0e423b7d5f35936781b13ebca
Thomas Varo <zalipsn@gmail.com>
2019-07-03 16:32:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.console.error("stop it");
-console.console.log("what the hell");
-console.console.warn("i dont get this");
+console.error("stop it");
+console.log("what the hell");
+console.warn("i dont get this");
