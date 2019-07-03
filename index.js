18ee9a959817370f029a2de790bc178af576d07d
Thomas Varo <zalipsn@gmail.com>
2019-07-03 16:25:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.console.error("what the hell am i doing")
-console.console.log("i dont get this");
-console.console.warn("STill dont get this");
+console.console.log("i dont get this")
+console.console.warn("STill dont get this")
